d81233a542040712f2e9d841a4f91c78b5708c7c
Maksym Stepanenko <maksym.stepanenko@opower.com>
2022-06-30 18:24:49 +0300

    Example benchmark modified. long vs int mul benchmark added

--- src/main/java/org/max/jmh/numbers/LongVsIntArithmeticBenchmark.java
+++ src/main/java/org/max/jmh/numbers/LongVsIntArithmeticBenchmark.java
@@ -1,6 +1,5 @@
-package org.max.jmh;
+package org.max.jmh.numbers;
 
-import java.util.Arrays;
 import java.util.concurrent.ThreadLocalRandom;
 import java.util.concurrent.TimeUnit;
 import org.openjdk.jmh.annotations.Benchmark;
@@ -12,68 +11,41 @@ import org.openjdk.jmh.annotations.OutputTimeUnit;
 import org.openjdk.jmh.annotations.Scope;
 import org.openjdk.jmh.annotations.State;
 import org.openjdk.jmh.annotations.Warmup;
-import org.openjdk.jmh.infra.Blackhole;
 import org.openjdk.jmh.runner.Runner;
 import org.openjdk.jmh.runner.RunnerException;
 import org.openjdk.jmh.runner.options.Options;
 import org.openjdk.jmh.runner.options.OptionsBuilder;
 
 /**
- * More detailed JMH examples can be found here:
- * https://github.com/openjdk/jmh/tree/master/jmh-samples/src/main/java/org/openjdk/jmh/samples
+ * Benchmark ints vs longs multiplication.
  */
 @Fork(1)
 @BenchmarkMode(Mode.AverageTime)
 @OutputTimeUnit(TimeUnit.NANOSECONDS)
 @Warmup(iterations = 5, time = 1000, timeUnit = TimeUnit.MILLISECONDS)
 @Measurement(iterations = 5, time = 1000, timeUnit = TimeUnit.MILLISECONDS)
-public class ExampleBenchmark {
+public class LongVsIntArithmeticBenchmark {
 
     @State(Scope.Benchmark)
     public static class BenchmarkState {
-        final double PI = Math.PI;
-    }
-
-    @State(Scope.Thread)
-    public static class ThreadState {
         final ThreadLocalRandom rand = ThreadLocalRandom.current();
-        private int[] arr1 = generateRandomArray();
-        private int[] arr2 = Arrays.copyOf(arr1, arr1.length);
-        ;
+        final int int1 = rand.nextInt(1_000_000);
+        final int int2 = rand.nextInt(1_000_000);
 
-        private int[] generateRandomArray() {
-
-            int[] arr = new int[10_000];
-
-            for (int i = 0; i < arr.length; ++i) {
-                arr[i] = rand.nextInt();
-            }
-
-            return arr;
-        }
-
-    }
-
-
-    @Benchmark
-    public void classicForLoop(BenchmarkState benchmarkState, ThreadState threadState, Blackhole bh) {
-        int sum = 0;
-        for (int i = 0; i < threadState.arr1.length; ++i) {
-            sum += (threadState.arr1[i] * benchmarkState.PI);
-        }
-
-        bh.consume(sum);
+        final long long1 = rand.nextLong(1_000_000);
+        final long long2 = rand.nextLong(1_000_000);
     }
 
     @Benchmark
-    public void foreach(BenchmarkState benchmarkState, ThreadState threadState, Blackhole bh) {
-        int sum = 0;
-        for (int val : threadState.arr2) {
-            sum += (val * benchmarkState.PI);
-        }
-
-        bh.consume(sum);
+    public int mulInts(BenchmarkState state) {
+        return state.int1 * state.int2;
     }
+
+    @Benchmark
+    public long mulLongs(BenchmarkState state) {
+        return state.long1 * state.long2;
+    }
+
 
     /*
      * ============================== HOW TO RUN THIS TEST: ====================================
@@ -84,12 +56,12 @@ public class ExampleBenchmark {
      *
      * a) Via the command line:
      *    $ ./mvnw clean package
-     *    $ java -jar target/benchmarks.jar ExampleBenchmark
+     *    $ java -jar target/benchmarks.jar LongVsIntArithmeticBenchmark
      */
 
     public static void main(String[] args) throws RunnerException {
         Options opt = new OptionsBuilder()
-            .include(ExampleBenchmark.class.getSimpleName())
+            .include(LongVsIntArithmeticBenchmark.class.getSimpleName())
 //            .threads(Runtime.getRuntime().availableProcessors())
 //            .jvmArgs("-ea")
             .build();
